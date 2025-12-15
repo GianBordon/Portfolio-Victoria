1d4679b858930e2d395d76c5f2eccfc81bc9005d
Gianfranco Bordon <shiammdp21@gmail.com>
2025-12-15 20:39:41 +0100

    Agregar fotos,paginas,cambio fuentes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"], // ajusta si hace falta
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
